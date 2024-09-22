c2a7a396014d2fa023c9099bd832d39a30a533de
Chyro03 <santiboca27@hotmail.com>
2024-09-22 14:38:30 -0500

    Feat Primera Seccion Inicio terminada

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import './style.css'
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
