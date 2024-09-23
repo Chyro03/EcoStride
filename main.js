8cf80875f0522f79d7358b3a7fe29e686d5ded6c
Chyro03 <santiboca27@hotmail.com>
2024-09-22 22:27:28 -0500

    Feat Creacion carrusel y correcion menu hamburguesa

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-import './style.css'
+import "./style.css";
+import "swiper/css/bundle";
+import Swiper from "swiper/bundle";
+const swiper6 = new Swiper(".mySwiper6", {
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      // origin: 'left center',
+      translate: [0, 0, -400],
+      // rotate: [0, 100, 0],
+    },
+    next: {
+      // origin: 'right center',
+      translate: ["100%", 0, 0],
+      // rotate: [0, -100, 0],
+    },
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction:true,
+  },
 
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+  speed: 1000,
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+  pagination: {
+    el: '.swiper-pagination', // Selector del contenedor de paginación
+    clickable: true,          // Habilitar clic en los puntos de paginación
+  },
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
